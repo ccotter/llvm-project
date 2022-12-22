ef40b5617091b633e3aff7150dc40eb8aea2ebb1
Chris Cotter <ccotter14@bloomberg.net>
2022-12-22 15:38:04 -0600

    Fix test

--- clang-tools-extra/test/clang-tidy/checkers/modernize/suggest-move.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize/suggest-move.cpp
@@ -2,6 +2,33 @@
 
 namespace std {
 
+template <typename>
+struct remove_reference;
+
+template <typename _Tp>
+struct remove_reference {
+  typedef _Tp type;
+};
+
+template <typename _Tp>
+struct remove_reference<_Tp &> {
+  typedef _Tp type;
+};
+
+template <typename _Tp>
+struct remove_reference<_Tp &&> {
+  typedef _Tp type;
+};
+
+template <typename _Tp>
+constexpr typename std::remove_reference<_Tp>::type &&move(_Tp &&__t) noexcept {
+  return static_cast<typename remove_reference<_Tp>::type &&>(__t);
+}
+
+}
+
+namespace std {
+
 template <typename T> struct vector { // NOLINT
   vector();
   vector(const vector&);
@@ -18,8 +45,8 @@ void containers() {
   std::vector<int> Vs;
 
   std::vector<int> Vs2{Vs};
-  // CHECK-MESSAGES: :[[@LINE-1]]:6: warning: use std::move to avoid copy [modernize-suggest-move]
-  // CHECK-FIXES: std::vector<int> Vs2{Vs};
+  // CHECK-MESSAGES: :[[@LINE-1]]:24: warning: use std::move to avoid copy [modernize-suggest-move]
+  // CHECK-FIXES: std::vector<int> Vs2{std::move(Vs)};
 }
 
 void containers_that_dont_trigger() {
