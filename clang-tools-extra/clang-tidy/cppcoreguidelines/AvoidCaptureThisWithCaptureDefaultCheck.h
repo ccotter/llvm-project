0c38849d8390a7f37acd743b2419e0c6c02c60b6
Chris Cotter <ccotter14@bloomberg.net>
2023-01-07 20:13:38 +0800

    typo

--- clang-tools-extra/clang-tidy/cppcoreguidelines/AvoidCaptureThisWithCaptureDefaultCheck.h
+++ clang-tools-extra/clang-tidy/cppcoreguidelines/AvoidCaptureThisWithCaptureDefaultCheck.h
@@ -15,10 +15,10 @@ namespace clang {
 namespace tidy {
 namespace cppcoreguidelines {
 
-/// Capture defaults lambas defined in in member functions can be misleading
-/// about whether capturing data member is by value or reference. For example,
-/// [=] will capture local variables by value but member variables by
-/// reference. CppCoreGuideline F.54 suggests to always be explicit
+/// Capture defaults in lambas defined within member functions can be
+/// misleading about whether capturing data member is by value or reference.
+/// For example, [=] will capture local variables by value but member variables
+/// by reference. CppCoreGuideline F.54 suggests to always be explicit
 /// and never specify a capture default when also capturing this.
 ///
 /// For the user-facing documentation see:
